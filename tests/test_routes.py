697a05fd675696d98a1fde320e6bb570d45ad759
Sarah Stringer <sstringer@twilio.com>
2021-06-03 20:04:23 -0700

    Fix tests.

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,5 @@
 from unittest.mock import patch
+import app
 
 
 def test_index(app, client):
@@ -7,19 +8,26 @@ def test_index(app, client):
     assert 'Make a Call:' in res.get_data(as_text=True)
 
 
-def test_voice_has_to_parameter(app, client):
-    res = client.post('/voice', data=dict(To='+593XXXXXXX'))
+def test_voice_has_phone_parameter(app, client):
+    res = client.post('/voice', data=dict(phone='+593XXXXXXX'))
     assert res.status_code == 200
     assert '<Client>+593XXXXXXX</Client>' in res.get_data(as_text=True)
 
+@patch.object(app, "twilio_number", "+593XXXXXXX")
+@patch.object(app, "identity", "nezuko")
+def test_voice_has_to_parameter(app, client):
+    res = client.post('/voice', data=dict(To='+593XXXXXXX'))
+    assert res.status_code == 200
+    assert '<Client>nezuko</Client>' in res.get_data(as_text=True)
 
-def test_voice_has_not_to_parameter(app, client):
+
+def test_voice_has_no_phone_or_to_parameter(app, client):
     res = client.post('/voice')
     assert res.status_code == 200
     assert '<Say>Thanks for calling!</Say>' in res.get_data(as_text=True)
 
 
-@patch('app.fake', **{'user_name.return_value': 'nezuko'})
+@patch.object(app, "identity", "nezuko")
 def test_generate_token(app, client):
     res = client.get('/token')
     assert '"identity":"nezuko"' in res.get_data(as_text=True)
